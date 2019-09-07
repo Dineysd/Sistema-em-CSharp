7b21b5e1dce64928865b75d5d3682250613c017e
Dineysd <38842411+Dineysd@users.noreply.github.com>
2019-09-07 17:04:57 -0300

    conexao com o banco sql serve 2017

---
+++ sistemadbs.DAL/DataContexFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemadbs.DAL
+{
+    public class DataContexFactory
+    {
+        private static SistemaFormDataContext dataContext;
+        SistemaFormDataContext DataContext
+        {
+            get
+            {
+                if(dataContext == null)
+                    dataContext = new SistemaFormDataContext();
+                    return dataContext;
+                
+            }
+        }
+   
+    }
+}
